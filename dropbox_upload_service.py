ba35ea54126f4a75e598f857ff7928422f3d0974
Noah Baxter <noahasoft@gmail.com>
2015-09-24 23:21:52 -0700

    Queues asynchronously, with progress.

--- dropbox_upload_service.py
+++ dropbox_upload_service.py
@@ -2,6 +2,7 @@ from bottle import HTTPResponse, route, run, template
 import os
 import os.path
 import shutil
+import threading
 
 
 PORT = 9999
@@ -14,7 +15,11 @@ filepaths_being_queued = frozenset()
 
 @route('/<filepath:path>')
 def upload(filepath):
+    global filepaths_being_queued
+    
     source_filepath = os.path.join(SOURCE_DIRPATH, filepath)
+    destination_filepath = os.path.join(DESTINATION_DIRPATH, filepath)
+    
     if not os.path.isfile(source_filepath):
         return HTTPResponse(
             status=404,
@@ -22,21 +27,46 @@ def upload(filepath):
             headers={'Content-Type': 'text/plain'}
         )
     
-    destination_filepath = os.path.join(DESTINATION_DIRPATH, filepath)
-    if os.path.exists(destination_filepath):
+    if source_filepath in filepaths_being_queued:
+        copied_bytes = \
+            0 if not os.path.exists(destination_filepath) else \
+            os.path.getsize(destination_filepath)
+        total_bytes = os.path.getsize(source_filepath)
+        
         return HTTPResponse(
             status=409,
-            body='Already queued for upload.',
+            body='Queueing... (%d%%)' % round(copied_bytes*100/total_bytes),
             headers={'Content-Type': 'text/plain'}
         )
     
-    # Copy file immediately
-    os.makedirs(os.path.dirname(destination_filepath), exist_ok=True)
-    shutil.copy2(source_filepath, destination_filepath)
+    if os.path.exists(destination_filepath):
+        return HTTPResponse(
+            status=409,
+            body='Queued. (100%)',
+            headers={'Content-Type': 'text/plain'}
+        )
+    
+    def queue(source_filepath, destination_filepath):
+        global filepaths_being_queued
+        
+        os.makedirs(os.path.dirname(destination_filepath), exist_ok=True)
+        shutil.copy2(source_filepath, destination_filepath)
+        
+        # Unlock
+        filepaths_being_queued = filepaths_being_queued - frozenset([source_filepath])
+    
+    # Lock
+    filepaths_being_queued = filepaths_being_queued | frozenset([source_filepath])
+    
+    # Copy file asynchronously
+    threading.Thread(
+        target=queue,
+        args=(source_filepath, destination_filepath)
+    ).start()
     
     return HTTPResponse(
         status=200,
-        body='Queued for upload.',
+        body='Queueing... (0%)',
         headers={'Content-Type': 'text/plain'}
     )
 
